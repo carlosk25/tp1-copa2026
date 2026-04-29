3a735fad4178cfe2eebee13b27fdf42cf4c0916a
danielj789 <danieljorge200247@gmail.com>
2026-04-29 12:03:32 -0300

    adicionar dps ao CopaApp (login)

--- src/main/java/br/unb/cic0197/copa2026/app/RunLogin.java
+++ src/main/java/br/unb/cic0197/copa2026/app/RunLogin.java
@@ -1,1 +1,13 @@
+package br.unb.cic0197.copa2026.app;
 
+import br.unb.cic0197.copa2026.view.TelaLogin; // Import alterado
+import javax.swing.*;
+
+public class RunLogin {
+    public static void main(String[] args) {
+            SwingUtilities.invokeLater(() -> {
+            TelaLogin tela = new TelaLogin();
+            tela.setVisible(true);
+        });
+    }
+}
