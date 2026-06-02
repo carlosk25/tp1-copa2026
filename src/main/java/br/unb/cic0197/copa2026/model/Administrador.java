504632efb49f371f167e8c35e9e9a6bd5d06e702
danielj789 <danieljorge200247@gmail.com>
2026-06-02 16:46:46 -0300

    Create Administrador.java

---
+++ src/main/java/br/unb/cic0197/copa2026/model/Administrador.java
@@ -0,0 +1,21 @@
+package br.unb.cic0197.copa2026.model;
+
+
+public class Administrador extends Usuario {
+    private static final long serialVersionUID = 1L;
+
+    public Administrador(String nome, String email, String senha, String dataNascimento) {
+        super(nome, email, senha, dataNascimento);
+    }
+
+    @Override
+    public String getTipoPerfil() {
+        return "Administrador";
+    }
+
+    @Override
+    public String obterDadosMetricaConsolidada() {
+        // Implementação polimórfica dos detalhes funcionais
+        return "Acesso irrestrito: Auditoria de Logs e Modificações de Infraestrutura.";
+    }
+}
