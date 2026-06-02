8172adc237d7f1537a1d6c90d60150272205d542
danielj789 <danieljorge200247@gmail.com>
2026-06-02 16:42:32 -0300

    Create Usuario.java

---
+++ src/main/java/br/unb/cic0197/copa2026/model/Usuario.java
@@ -0,0 +1,33 @@
+package br.unb.cic0197.copa2026.model;
+
+import java.io.Serializable;
+
+public abstract class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String dataNascimento;
+    private boolean primeiroAcesso; // Nova flag para controle de alteração de senha
+
+    public Usuario(String nome, String email, String senha, String dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+        this.primeiroAcesso = true; // Por padrão, usuários novos começam no primeiro acesso
+    }
+
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public String getDataNascimento() { return dataNascimento; }
+
+    public boolean isPrimeiroAcesso() { return primeiroAcesso; }
+    public void setPrimeiroAcesso(boolean primeiroAcesso) { this.primeiroAcesso = primeiroAcesso; }
+
+    public abstract String getTipoPerfil();
+    public abstract String obterDadosMetricaConsolidada();
+}
