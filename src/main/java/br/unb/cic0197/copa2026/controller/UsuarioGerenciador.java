5c7c7fa891e676378db52cb65518d81662c4eb58
danielj789 <danieljorge200247@gmail.com>
2026-06-10 23:35:13 -0300

    Update UsuarioGerenciador.java

--- src/main/java/br/unb/cic0197/copa2026/controller/UsuarioGerenciador.java
+++ src/main/java/br/unb/cic0197/copa2026/controller/UsuarioGerenciador.java
@@ -1,15 +1,15 @@
 package br.unb.cic0197.copa2026.controller;
 
-import java.util.List;
-
-import br.unb.cic0197.copa2026.exception.UsuarioInvalidoException;
-import br.unb.cic0197.copa2026.model.SolicitacaoCadastro;
 import br.unb.cic0197.copa2026.model.Usuario;
+import br.unb.cic0197.copa2026.model.Administrador;
+import br.unb.cic0197.copa2026.model.SolicitacaoCadastro;
 import br.unb.cic0197.copa2026.service.UsuarioService;
+import java.util.List;
+import br.unb.cic0197.copa2026.exception.UsuarioInvalidoException;
 
 
 public class UsuarioGerenciador {
-  
+
     private static final UsuarioService usuarioService = new UsuarioService();
 
     public static List<Usuario> listarTodos() {
@@ -32,13 +32,31 @@ public class UsuarioGerenciador {
         usuarioService.atualizarSenhaPrimeiroAcesso(email, novaSenha);
     }
 
-    public static void editarUsuario(String email, String nome, String emailRepetido, String dataNascimento, String senha, String tipoPerfil) {
-      
-        boolean primeiroAcesso = false;
-        usuarioService.editarUsuario(nome, email, senha, dataNascimento, primeiroAcesso, tipoPerfil);
+    public static void editarUsuario(String emailOriginal, String nome, String emailNovo, String dataNascimento, String senha, String tipoPerfil) {
+        usuarioService.editarUsuario(nome, emailOriginal, senha, dataNascimento, false, tipoPerfil);
     }
 
     public static void excluirUsuario(String email) {
         usuarioService.excluirUsuario(email);
     }
+
+
+    public static List<SolicitacaoCadastro> obterTodasSolicitacoes() {
+        return usuarioService.obterTodasSolicitacoes();
+    }
+
+    public static List<Usuario> obterTodosUsuarios() {
+        return usuarioService.obtertodas();
+    }
+
+    public static void adicionarSolicitacao(br.unb.cic0197.copa2026.model.SolicitacaoCadastro novaSolicitacao) throws Exception {
+
+        usuarioService.cadastrarSolicitacao(novaSolicitacao);
+    }
+
+    public static void reprovarSolicitacao(SolicitacaoCadastro solicitacao) {
+
+        usuarioService.reprovarSolicitacao(solicitacao);
+    }
 }
+
