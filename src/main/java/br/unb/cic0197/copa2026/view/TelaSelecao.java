fea65dbfbf4b37fd9394c524e0c5509a779a1194
danielj789 <danieljorge200247@gmail.com>
2026-06-15 20:14:21 -0300

    Update TelaSelecao.java

--- src/main/java/br/unb/cic0197/copa2026/view/TelaSelecao.java
+++ src/main/java/br/unb/cic0197/copa2026/view/TelaSelecao.java
@@ -289,7 +289,7 @@ public class TelaSelecao extends JPanel {
                         "Sucesso",
                         JOptionPane.INFORMATION_MESSAGE);
             }
-
+            app.recarregarPartidas();
             atualizarTabela(selecaoController.listarSelecoes());
             limparCampos();
 
@@ -320,6 +320,7 @@ public class TelaSelecao extends JPanel {
         if (confirmacao == JOptionPane.YES_OPTION) {
             try {
                 selecaoController.removerSelecao(selecaoSelecionada);
+                app.recarregarPartidas();
                 atualizarTabela(selecaoController.listarSelecoes());
                 limparCampos();
                 JOptionPane.showMessageDialog(this,
